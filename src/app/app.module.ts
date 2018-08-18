80e557f84d59a8d2d8cb85fa3d50d3a219bbe193
Oleg Bloshenkin <obloshenkin@playtika.com>
2018-08-18 20:34:31 +0300

    Create angular auction via tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+// import { ApplicationComponent } from './components/application/application.component';
+import { StarsComponent } from './components/stars/stars.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { HomeComponent } from './components/home/home.component';
+
+// Services
+import { ProductService } from './services/product.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent,
+    // ApplicationComponent,
+    StarsComponent,
+    NavbarComponent,
+    SearchComponent,
+    FooterComponent,
+    CarouselComponent,
+    ProductDetailComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ProductService,
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/application/application.component.ts
@@ -0,0 +1,9 @@
+// import {Component, ViewEncapsulation} from '@angular/core';
+
+// @Component({
+//   selector: 'app-application',
+//   templateUrl: './application.component.html',
+//   styleUrls: ['./application.component.css'],
+//   encapsulation: ViewEncapsulation.None
+// })
+// export class ApplicationComponent {}
